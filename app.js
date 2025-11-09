3f59e59aaca8e9f43dd63fd7a9b1408edc50b66c
Yaekirua <111132084+Yaekirua@users.noreply.github.com>
2025-11-09 22:33:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const io = new Server(server);
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'waja9006',
+    password: 'youPassword',
     database: 'confession_db',
 });
 
